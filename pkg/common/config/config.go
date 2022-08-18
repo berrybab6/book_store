7c5d24a852e217792364c834c26ece25f08910e8
mishassefa <bedatuassefa@gmail.com>
2022-08-19 02:41:41 +0300

    add configurations

---
+++ pkg/common/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Port  string `mapstructure:"PORT"`
+	DBUrl string `mapstructure:"DB_URL"`
+}
+
+func LoadConfig() (c Config, err error) {
+	// viper.AddConfigPath("./pkg/common/config/envs")
+	viper.SetConfigName("dev")
+	viper.SetConfigType("env")
+	viper.SetConfigFile("./pkg/common/config/envs/.env")
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+
+	if err != nil {
+		return
+	}
+
+	fmt.Println(viper.ConfigFileUsed())
+	viper.Unmarshal(&c)
+	return
+}
