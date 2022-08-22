14b86234b53d79147501cb0bd25193a08f6802ec
mishassefa <bedatuassefa@gmail.com>
2022-08-22 12:44:26 +0300

    add check password

--- pkg/common/models/user.go
+++ pkg/common/models/user.go
@@ -1,8 +1,28 @@
 package models
 
+import "golang.org/x/crypto/bcrypt"
+
 type User struct {
 	Id       uint   `json:"id" gorm:"primaryKey"`
 	Name     string `json:"name"`
+	Username string `json:"username" gorm:"unique"`
 	Email    string `json:"email" gorm:"unique"`
-	Password []byte `json:"-"`
+	Password string `json:"password"`
+}
+
+func (user *User) HashPassword(thePassword string) error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(thePassword), 14)
+	if err != nil {
+		return err
+	}
+	user.Password = string(bytes)
+	return nil
+}
+
+func (user *User) CheckPassword(thePassword string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(thePassword))
+	if err != nil {
+		return err
+	}
+	return nil
 }
