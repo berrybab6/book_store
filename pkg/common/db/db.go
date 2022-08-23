fbcaf9930030b2fd6a8ffbd593165a6f03eb1730
mishassefa <bedatuassefa@gmail.com>
2022-08-23 12:15:06 +0300

    AutoMigrate Author model

--- pkg/common/db/db.go
+++ pkg/common/db/db.go
@@ -16,5 +16,14 @@ func Init(url string) *gorm.DB {
 	}
 	db.AutoMigrate(&models.Book{})
 	db.AutoMigrate(&models.User{})
+	db.AutoMigrate(&models.Author{})
+	// db.Model(&models.Author{})
+
+	db.Migrator().CreateConstraint(&models.Book{}, "Authors")
+	db.Migrator().CreateConstraint(&models.Book{}, "fk_books_author")
+
+	// db.Model(&models.Book{}).AddForeignKey("author_id", "authors(id)", "RESTRICT", "RESTRICT")
+	// db.Model(&mode{}).AddForeignKey("profile_id", "profiles(id)", "NO ACTION", "NO ACTION")
+
 	return db
 }
