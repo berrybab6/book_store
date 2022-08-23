3909bdf7553e291b1ad9aa96757704a33954fecf
mishassefa <bedatuassefa@gmail.com>
2022-08-23 17:08:51 +0300

    update forgot password

--- pkg/users/forgot_password.go
+++ pkg/users/forgot_password.go
@@ -1,12 +1,33 @@
 package users
 
 import (
+	"math/rand"
+	"time"
+
 	"github.com/berrybab6/MovieGo/pkg/common/models"
 	"github.com/gofiber/fiber/v2"
 )
 
 type ForgotPassReqBody struct {
 	Email string `gorm:"email"`
+}
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+func StringWithCharset(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func RandomString(length int) string {
+	return StringWithCharset(length, charset)
 }
 
 func (h handler) ForgotPassword(c *fiber.Ctx) error {
@@ -21,7 +42,10 @@ func (h handler) ForgotPassword(c *fiber.Ctx) error {
 		return fiber.NewError(fiber.StatusBadRequest, resp.Error.Error())
 	}
 
+	user.ResetToken = RandomString(23)
 	//######### Generate Reset Token Here
-
-	return nil
+	h.DB.Save(&user)
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"reset_token": &user.ResetToken,
+	})
 }
