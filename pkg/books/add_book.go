562830066dd932faf2373a643fe0640882515b2d
mishassefa <bedatuassefa@gmail.com>
2022-08-25 12:37:54 +0300

    update add_book.go

--- pkg/books/add_book.go
+++ pkg/books/add_book.go
@@ -5,12 +5,25 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// AddBookRequestBody MovieGo
 type AddBookRequestBody struct {
 	Title       string `gorm:"title"`
 	AuthorID    uint   `gorm:"authorid"`
 	Description string `gorm:"description"`
 }
 
+// AddBook godoc
+// @Summary      Add a Book
+// @Description  add by json book
+// @Tags         books
+// @Accept       json
+// @Produce      json
+// @Param        book  body      AddBookRequestBody  true  "Add Book"
+// @Success      201      {object}  models.Book
+// @Failure      400      {object}  httputil.HTTPError
+// @Failure      404      {object}  httputil.HTTPError
+// @Failure      500      {object}  httputil.HTTPError
+// @Router       /books [post]
 func (h handler) AddBook(c *fiber.Ctx) error {
 	body := AddBookRequestBody{}
 	if err := c.BodyParser(&body); err != nil {
