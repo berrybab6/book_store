c036b08e6e1b9377b44dc07b17c235fa62244ac7
mishassefa <bedatuassefa@gmail.com>
2022-08-27 10:55:24 +0300

    update forgotpassword controller

--- pkg/users/forgot_password.go
+++ pkg/users/forgot_password.go
@@ -2,12 +2,16 @@ package users
 
 import (
 	"math/rand"
+	"strings"
 	"time"
 
 	"github.com/berrybab6/MovieGo/pkg/common/models"
+	"github.com/berrybab6/MovieGo/pkg/common/utils"
+
 	"github.com/gofiber/fiber/v2"
 )
 
+// ForgotPassReqBody MovieGo
 type ForgotPassReqBody struct {
 	Email string `gorm:"email"`
 }
@@ -18,6 +22,7 @@ const charset = "abcdefghijklmnopqrstuvwxyz" +
 var seededRand *rand.Rand = rand.New(
 	rand.NewSource(time.Now().UnixNano()))
 
+// StringWithCharset MovieGo
 func StringWithCharset(length int, charset string) string {
 	b := make([]byte, length)
 	for i := range b {
@@ -26,10 +31,23 @@ func StringWithCharset(length int, charset string) string {
 	return string(b)
 }
 
+// RandomString MovieGo
 func RandomString(length int) string {
 	return StringWithCharset(length, charset)
 }
 
+// ForgotPassword godoc
+// @Summary      Forgot Password getway
+// @Description  change user password
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Param      	user    body      ForgotPassReqBody  true  "Forgot Password"
+// @Success      200      {object}  models.User
+// @Failure      400      {object}  httputil.HTTPError
+// @Failure      404      {object}  httputil.HTTPError
+// @Failure      500      {object}  httputil.HTTPError
+// @Router       /users/forgot [put]
 func (h handler) ForgotPassword(c *fiber.Ctx) error {
 	req := ForgotPassReqBody{}
 
@@ -45,6 +63,24 @@ func (h handler) ForgotPassword(c *fiber.Ctx) error {
 	user.ResetToken = RandomString(23)
 	//######### Generate Reset Token Here
 	h.DB.Save(&user)
+	var firstName = user.Name
+
+	if strings.Contains(firstName, " ") {
+		firstName = strings.Split(firstName, " ")[1]
+	}
+	emailData := utils.EmailData{
+		// URL:       config.Origin + "/forgotPassword/" + user.ResetToken,
+		URL:       "http://localhost:8080/users/forgotPassword/" + user.ResetToken,
+		FirstName: firstName,
+		Subject:   "Your password reset token (valid for 10min)",
+	}
+
+	err := utils.SendEmail(&user, &emailData, "resetPassword.html")
+
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadGateway, err.Error())
+
+	}
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		"reset_token": &user.ResetToken,
 	})
