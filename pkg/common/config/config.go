2da794b7f89b8fa67659c35f5f87218a20f70e51
mishassefa <bedatuassefa@gmail.com>
2022-08-27 10:53:30 +0300

    update config.go file

--- pkg/common/config/config.go
+++ pkg/common/config/config.go
@@ -8,8 +8,22 @@ import (
 
 // Config MovieGo
 type Config struct {
-	Port  string `mapstructure:"PORT"`
-	DBUrl string `mapstructure:"DB_URL"`
+	Port       string `mapstructure:"PORT"`
+	DBUrl      string `mapstructure:"DB_URL"`
+	ApiSecret  string `mapstructure:"API_SECRET"`
+	DBHost     string `mapstructure:"DB_HOST"`
+	DBUser     string `mapstructure:"DB_USER"`
+	DBDriver   string `mapstructure:"DB_DRIVER"`
+	DBPassword string `mapstructure:"DB_PASSWORD"`
+	DBName     string `mapstructure:"DB_NAME"`
+	DBPort     string `mapstructure:"DB_PORT"`
+
+	// SMTP configuration
+	EmailFrom string `mapstructure:"EMAIL_FROM"`
+	SMTPHost  string `mapstructure:"SMTP_HOST"`
+	SMTPPass  string `mapstructure:"SMTP_PASS"`
+	SMTPUser  string `mapstructure:"SMTP_USER"`
+	SMTPPort  int    `mapstructure:"SMTP_PORT"`
 }
 
 // LoadConfig MovieGo
@@ -17,7 +31,9 @@ func LoadConfig() (c Config, err error) {
 	// viper.AddConfigPath("./pkg/common/config/envs")
 	viper.SetConfigName("dev")
 	viper.SetConfigType("env")
-	viper.SetConfigFile("./pkg/common/config/envs/.env")
+	// viper.SetConfigFile("./pkg/common/config/envs/.env")
+	viper.SetConfigFile(".env")
+
 	viper.AutomaticEnv()
 
 	err = viper.ReadInConfig()
